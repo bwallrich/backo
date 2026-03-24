7e039e8a467240a3511398db61f64068f81fb5ee
Bertrand Wallrich <Bertrand.Wallrich@inria.fr>
2026-03-24 21:45:26 +0100

    fix loop_path.py linter error

--- backo/loop_path.py
+++ backo/loop_path.py
@@ -22,30 +22,30 @@ class LoopPath:
         self._path = []
         self.max_loop = max_loop
 
-    def is_loop(self, collection_name: str, id: str, path: str) -> bool:
+    def is_loop(self, collection_name: str, _id: str, path: str) -> bool:
         """detect if already in the path
 
         :param collection_name: the name of the collection
         :type collection_name: str
-        :param id: the id of the object in the collection
-        :type id: str
+        :param _id: the _id of the object in the collection
+        :type _id: str
         :param path: the path (ex : $.site )
         :type path: str
         :return: True if seen
         :rtype: bool
         """
         for m_path in self._path:
-            if m_path == (collection_name, id, path):
+            if m_path == (collection_name, _id, path):
                 return True
         return False
 
-    def append(self, collection_name: str, id: str, path: str) -> None:
-        """Append to the path a tuple (collection, id, path )
+    def append(self, collection_name: str, _id: str, path: str) -> None:
+        """Append to the path a tuple (collection, _id, path )
 
         :param collection_name: the name of the collection
         :type collection_name: str
-        :param id: the id of the object in the collection
-        :type id: str
+        :param _id: the _id of the object in the collection
+        :type _id: str
         :param path: the path (ex : $.site )
         :type path: str
         """
@@ -53,7 +53,7 @@ class LoopPath:
         if len(self._path) > self.max_loop:
             raise Error(
                 ErrorType.MAX_LOOP,
-                f"Loop max detected for ( {collection_name}, {id}, {path})",
+                f"Loop max detected for ( {collection_name}, {_id}, {path})",
             )
 
-        self._path.append((collection_name, id, path))
+        self._path.append((collection_name, _id, path))
