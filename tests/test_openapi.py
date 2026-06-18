e1b635cb491716564b5e64e7a4b2ccdcbaa7ce7b
Théo BL <theo.biasutto-lervat@inria.fr>
2026-06-18 11:52:24 +0200

    test(openapi): split test, one to check the route, another to check the json content

--- tests/test_openapi.py
+++ tests/test_openapi.py
@@ -3,29 +3,10 @@ Test for OpenAPI specification generation.
 """
 
 import unittest
-from typing import Callable
 
 from flask import Flask
-from flask.testing import FlaskClient
 
 from backo import Backoffice
-
-
-def _build_test_client(backoffice_generator: Callable[[], Backoffice]) -> FlaskClient:
-    """
-    Build a test client for the given backoffice generator.
-
-    :param backoffice_generator: A callable that returns a Backoffice instance.
-    :type backoffice_generator: Callable[[], Backoffice]
-    :return: A Flask test client.
-    :rtype: FlaskClient
-    """
-    app = Flask(__name__)
-    backoffice = backoffice_generator()
-    backoffice.build_routes(app)
-    ctx = app.app_context()
-    ctx.push()
-    return app.test_client()
 
 
 class TestOpenAPI(unittest.TestCase):
@@ -33,14 +14,33 @@ class TestOpenAPI(unittest.TestCase):
     Test OpenAPI specification generation.
     """
 
+    def test_openapi_route(self):
+        """
+        Test if the /openapi route is properly registered.
+        """
+        # Setup Test Flask app and Backoffice
+        app = Flask(__name__)
+        backoffice = Backoffice("app")
+        backoffice.build_routes(app)
+        ctx = app.app_context()
+        ctx.push()
+        client = app.test_client()
+        # Test the /openapi route
+        response = client.get("/app/openapi")
+        # Ensure the response is successful and contains JSON data with correct MIME type
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.get_json())
+
     def test_openapi_basic(self):
         """
-        Test the generation of OpenAPI specification.
+        Test the basic OpenAPI specification generation for an empty backoffice.
+        - OpenAPI version 3.1.0
+        - 3 default schemas: backo-meta, json-patch, backo-filter
+        - no routes
         """
-        client = _build_test_client(lambda: Backoffice("app"))
-        response = client.get("/app/openapi")  # Trigger the OpenAPI generation
-        self.assertEqual(response.status_code, 200)
-        spec = response.get_json()
+        # Setup Test Backoffice
+        backoffice = Backoffice("app")
+        spec = backoffice.get_openapi()
         # Expecting OpenAPI version 3.1.0
         self.assertEqual(spec["openapi"], "3.1.0")
         # Expecting 3 default schemas: backo-meta, json-patch, backo-filter
